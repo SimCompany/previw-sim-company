b45644c4749fcb27c5340ceacf85d45ca6263591
Matteus Isaque <matteus.isaque28@gmail.com>
2024-01-16 10:50:34 -0300

    alt

--- src/components/ButtonForm/FormButton.tsx
+++ src/components/ButtonForm/FormButton.tsx
@@ -38,7 +38,7 @@ export default function FormButton({ plane, router, bgColorProp = 'Green.300', c
     const data = {
       domain: 'www.simcompany.com.br',
       // emailTo: ['luana@simcompany.com.br', 'hubner@simcompany.com.br', 'edgard@simcompany.com.br'],
-      emailTo: ["matteus.isaque28@gmail.com"],
+      emailTo: ["matteus.isaque29@gmail.com"],
       title: 'Formulário de contato sim company',
       name,
       email,
